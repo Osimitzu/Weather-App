0832067ab3880d6649fac619193c1158b1bcf265
Osimitzu <angel_aocc@hotmail.com>
2023-10-30 19:50:47 -0600

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="w-fit">
-      <a href="https://osirisdev.netlify.app" target="_blank">
+      <a href="https://osiriscontreras.com" target="_blank">
         <img src="/gorilla.png" alt="githubProfile" className="w-12" />
       </a>
     </div>
